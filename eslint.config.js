9a476bb96f09dcf289eee635713680a099634d30
josephcrozet <joseph.c.bandera@gmail.com>
2026-04-24 23:48:57 +0200

    Migrate import sorting to Biome assist and fix alias imports

--- eslint.config.js
+++ eslint.config.js
@@ -8,40 +8,7 @@ export default antfu(
 	},
 	{
 		rules: {
-			'perfectionist/sort-imports': [
-				'error',
-				{
-					internalPattern: [
-						'^@assets/.+',
-						'^@components/.+',
-						'^@layouts/.+',
-						'^@styles/.+',
-					],
-					groups: [
-						'type-astro',
-						'astro',
-						'type-import',
-						['value-builtin', 'value-external'],
-						'type-internal',
-						'value-internal',
-						['type-parent', 'type-sibling', 'type-index'],
-						['value-parent', 'value-sibling', 'value-index'],
-						'ts-equals-import',
-						'unknown',
-					],
-					customGroups: [
-						{
-							groupName: 'type-astro',
-							modifiers: ['type'],
-							elementNamePattern: ['^astro$', '^astro:.+', '^astro/.+'],
-						},
-						{
-							groupName: 'astro',
-							elementNamePattern: ['^astro$', '^astro:.+', '^astro/.+'],
-						},
-					],
-				},
-			],
+			'perfectionist/sort-imports': 'off',
 		},
 	},
 );
