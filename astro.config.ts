2ca9711740743193a3f41c59919ad20ab4df61e5
josephcrozet <joseph.c.bandera@gmail.com>
2026-04-29 16:42:38 +0200

    Migrate config files from .mjs to .js and .ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
 	site: 'https://lastquestion.dev',
 	integrations: [mdx(), sitemap()],
--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default defineConfig([
-	globalIgnores(['**/*.js', '**/*.cjs', '**/*.mjs', '.astro/', 'dist/']),
+	globalIgnores(['**/*.js', '.astro/', 'dist/']),
 	...eslintPluginAstro.configs.recommended,
 	{
 		files: ['**/*.astro'],
