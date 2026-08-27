a86a5b7c568f605c6b99b583f6bbdc09d3c75f6e
Joseph Crozet <123495528+josephcrozet@users.noreply.github.com>
2026-08-26 17:28:58 -0700

    Record Playwright traces on first retry (#96)

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
 	retries: process.env.CI ? 2 : 0,
 	use: {
 		baseURL: 'http://localhost:4321',
+		trace: 'on-first-retry',
 	},
 	webServer: {
 		command: process.env.CI ? 'bun run astro preview' : 'bun run astro dev',
